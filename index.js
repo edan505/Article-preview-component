21d8b73ab62b1be8f77af09a8fb45b80a0f2b41e
Edwin Mejia <60832962+edan505@users.noreply.github.com>
2020-11-12 21:58:56 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-document.querySelectorAll(".share-container")[0].addEventListener("click", function(){
-    if (window.innerWidth > 975){
-    document.querySelectorAll(".share-popup")[0].classList.toggle("share-popup-clicked");
-    document.querySelectorAll(".share-container")[0].classList.toggle("share-container-clicked");
-    document.querySelector("path").classList.toggle("button-fill-clicked");
-    } else{
-        alert("mobile viewport");
-    }
+
+let buttonArray = document.querySelectorAll(".share-container");
+let sharePopup = document.querySelectorAll(".share-popup");
+let path = document.querySelector("path");
+let textFooter = document.querySelector(".text-div-footer");
+let mobileFooter = document.querySelector(".footer-mobile-div");
+
+for(let i = 0; i <= buttonArray.length - 1; i++ ){
+    buttonArray[i].addEventListener("click", function () {
+        if (window.innerWidth > 975) {
+            sharePopup[0].classList.toggle("share-popup-clicked");
+            buttonArray[0].classList.toggle("share-container-clicked");
+            path.classList.toggle("button-fill-clicked");
+        } else {
+            textFooter.classList.toggle("text-div-footer-hidden");
+            mobileFooter.classList.toggle("footer-mobile-div-clicked");
+        }
     
-});
+    });
+}
